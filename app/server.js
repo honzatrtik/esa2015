7a4b44dccf9ae37e69f8425f055ba7c847bf5d22
trta <honza.trtik@gmail.com>
2015-07-15 22:49:41 +0200

    ga fix order of js

--- app/server.js
+++ app/server.js
@@ -44,13 +44,13 @@ function renderPage(html, title, state) {
         <script>
           ${state}
         </script>
-        <script src="/build/client.js"></script>
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
         </script>
+        <script src="/build/client.js"></script>
         </body>
         </html>`;
 }
