df642ea94a0b5eaa61220d13314a9491c6fb4625
trta <honza.trtik@gmail.com>
2015-08-11 22:40:59 +0200

    import autoru #2

--- src/Esa/Command/ImportAuthorsCommand.php
+++ src/Esa/Command/ImportAuthorsCommand.php
@@ -54,7 +54,7 @@ class ImportAuthorsCommand extends Command
 
 	protected function makeHash($data)
 	{
-		return sha1($data['submitting_author_ID']);
+		return sha1(join(',', func_get_args()));
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
@@ -69,7 +69,7 @@ class ImportAuthorsCommand extends Command
 
 		foreach($it as $data)
 		{
-			$hash = $this->makeHash($data);
+			$hash = $this->makeHash($data['sa_firstname'], $data['sa_name']);
 
 			$personId = intval($data['submitting_author_ID']);
 			$presentationId = $data['paperID'];
@@ -81,24 +81,26 @@ class ImportAuthorsCommand extends Command
 			}
 			*/
 
+			$firstChar = mb_substr($data['sa_name'], 0, 1, 'UTF8');
+
+			$lastName = isset($data['sa_name'])
+				? $data['sa_name']
+				: trim(explode(',', $data['submitting_author'])[0]);
+
+			$authorData = [
+				'person_id' => $personId ?: NULL,
+				'author_hash' => $hash,
+				'first_char' => $firstChar,
+				'first_name' => $data['sa_firstname'] ?: NULL,
+				'last_name' =>  $lastName ?: NULL,
+				'email' => $data['sa_email'],
+				'name' => $data['submitting_author'],
+				'organisation' => join(', ', [$data['sa_organisation'], $data['sa_country']]),
+			];
+
 			if (!isset($authorsCache[$hash]))
 			{
-				$firstChar = mb_substr($data['sa_name'], 0, 1, 'UTF8');
 
-				$lastName = isset($data['sa_name'])
-					? $data['sa_name']
-					: trim(explode(',', $data['submitting_author'])[0]);
-
-				$authorData = [
-					'person_id' => $personId ?: NULL,
-					'author_hash' => $hash,
-					'first_char' => $firstChar,
-					'first_name' => $data['sa_firstname'] ?: NULL,
-					'last_name' =>  $lastName ?: NULL,
-					'email' => $data['sa_email'],
-					'name' => $data['submitting_author'],
-					'organisation' => join(', ', [$data['sa_organisation'], $data['sa_country']]),
-				];
 
 				$authorsCache[$hash] = [
 					'authorData' => $authorData,
@@ -112,6 +114,48 @@ class ImportAuthorsCommand extends Command
 			{
 				$authorsCache[$hash]['presentationIds'][] = $presentationId;
 			}
+
+
+			$emails = array_map('trim', explode(',', $data['all_emails']));
+			foreach($this->getAuthors($data['authors'], $data['organisations']) as $index => $nameOrganisations)
+			{
+				$firstChar = mb_substr($nameOrganisations['name'], 0, 1, 'UTF8');
+
+				list($lastName, $firstName) = array_map('trim', (explode(',', $nameOrganisations['name'])));
+
+				$hash = $this->makeHash($firstName, $lastName);
+
+				$authorData = [
+					'person_id' => NULL,
+					'author_hash' => $hash,
+					'first_char' => $firstChar,
+					'first_name' => $firstName ?: NULL,
+					'last_name' =>  $lastName ?: NULL,
+					'email' => empty($emails[$index])
+						? sprintf('coauthor-%s@fakemail.com', $hash)
+						: $emails[$index],
+					'name' => $nameOrganisations['name'],
+					'organisation' => $nameOrganisations['organisation'],
+				];
+
+				if (!isset($authorsCache[$hash]))
+				{
+
+
+					$authorsCache[$hash] = [
+						'authorData' => $authorData,
+						'presentationIds' => [$presentationId],
+					];
+
+					$output->writeln(sprintf('Parsed: <info>[%s] %s, %s</info>.', $personId, $authorData['last_name'], $authorData['first_name']));
+
+				}
+				else
+				{
+					$authorsCache[$hash]['presentationIds'][] = $presentationId;
+				}
+			}
+
 
 		}
 
@@ -130,7 +174,7 @@ class ImportAuthorsCommand extends Command
 
 			$authorId = $this->db->query('SELECT lastval()')->fetchColumn();
 
-			foreach($author['presentationIds'] as $presentationId)
+			foreach(array_unique($author['presentationIds']) as $presentationId)
 			{
 				$exists = $this->db->fetchColumn('SELECT COUNT(*) FROM presentation WHERE id = ?', [$presentationId]);
 				if ($exists)
@@ -158,5 +202,60 @@ class ImportAuthorsCommand extends Command
 		{
 			$this->db->commit();
 		}
+	}
+
+
+	private function addData($authorData, $presentationIds)
+	{
+
+	}
+
+	private function getAuthors($n ,$o)
+	{
+		$authors = [];
+
+		$nameParts = array_map('trim', explode(';', $n));
+		$organisationParts = array_map('trim', explode(';', $o));
+
+
+		$organisations = [];
+		foreach($organisationParts as $part)
+		{
+			if (preg_match('/^\s*(\d+)\:\s*(.*)\s*$/', $part, $matches))
+			{
+				$organisations[$matches[1]] = $matches[2];
+			}
+			else
+			{
+				$organisations[] = $part;
+			}
+		}
+
+		foreach($nameParts as $part)
+		{
+			if (preg_match('/^\s*(.*)\s*\((.*)\)$/', $part, $matches))
+			{
+				$indexes = array_map('trim', explode(',', $matches[2]));
+				$authorOrganisations = [];
+				foreach($indexes as $index)
+				{
+					$authorOrganisations[] = isset($organisations[$index])
+						? $organisations[$index]
+						: $organisations[0];
+				}
+				$authors[] = [
+					'name' => $matches[1],
+					'organisation' => join('; ', $authorOrganisations),
+				];
+			}
+			else
+			{
+				$authors[] = [
+					'name' => $part,
+					'organisation' => $organisations[0],
+				];
+			}
+		}
+		return $authors;
 	}
 }
